7ab57f0127c397aa4abdd2f5fbdc041327b53444
Emranul Hoque <emranrakib01862@gmail.com>
2026-05-15 16:44:23 +0600

    updated

---
+++ lib/data/model/task_status_count.dart
@@ -0,0 +1,18 @@
+class taskstatuscountmodel {
+  String? sId;
+  int? sum;
+
+  taskstatuscountmodel({this.sId, this.sum});
+
+  taskstatuscountmodel.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    sum = json['sum'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['sum'] = this.sum;
+    return data;
+  }
+}
--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:task_manager/Widgets/screen_background.dart';
 import 'package:task_manager/screens/login_screen.dart';
 import 'package:task_manager/screens/sign_up_screen.dart';
@@ -13,7 +14,17 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
 
-  void _onTaplogin(){
+final GlobalKey<FormState> _formkey=GlobalKey<FormState>();
+final TextEditingController _emailController=TextEditingController();
+final TextEditingController _firstNameController=TextEditingController();
+final TextEditingController _lastNamemailController=TextEditingController();
+final TextEditingController _mobileController=TextEditingController();
+final TextEditingController _passwordController=TextEditingController();
+
+
+
+
+void _onTaplogin(){
     Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
   }
   @override
@@ -23,61 +34,83 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: ScreenBackground(
         child: Padding(
           padding: const EdgeInsets.all(30.0),
-          child: ScreenBackground(child: Column(
-            children: [
-              SizedBox(height: 150,),
-              Text("Join with Us",
-                style: Theme.of(context).textTheme.titleLarge,
+          child: ScreenBackground(
+            child: SingleChildScrollView(
+              child: Form(
+
+key: _formkey,
+
+
+                child: Column(
+                children: [
+                  SizedBox(height: 150,),
+                  Text("Join with Us",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  SizedBox(height: 25,),
+                        
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        hintText: 'Email'
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                        
+                  TextFormField(
+                    controller: _firstNameController,
+                    decoration: InputDecoration(
+                        hintText: 'First Name'
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                        
+                        
+                        
+                  TextFormField(
+                    controller: _lastNamemailController,
+                    decoration: InputDecoration(
+                        hintText: 'Last Name'
+                    ),
+                  ),
+
+
+
+                  TextFormField(
+                    controller: _mobileController,
+                    decoration: InputDecoration(
+                        hintText: 'Last Name'
+                    ),
+                  ),
+                        
+                        
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        
+                        hintText: 'Password'
+                    ),
+                  ),
+                  FilledButton(onPressed: (){}, child: Icon(Icons.arrow_circle_right_outlined)),
+                  SizedBox(height: 35),
+                  Center(
+                    child: RichText(text: TextSpan(
+                        text: "Have an account?",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600),
+                        children: [
+                          TextSpan(
+                              text: 'Login',style: TextStyle(color: AppColors.pcolor,fontWeight:FontWeight.bold),
+                              recognizer: TapGestureRecognizer()..onTap=_onTaplogin
+                          )
+                        ]
+                        
+                    )),
+                  )
+                        
+                ],
+                          ),
               ),
-              SizedBox(height: 25,),
-        
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Email'
-                ),
-              ),
-              SizedBox(height: 10,),
-        
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'First Name'
-                ),
-              ),
-              SizedBox(height: 10,),
-        
-        
-        
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Last Name'
-                ),
-              ),
-        
-        
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-        
-                    hintText: 'Password'
-                ),
-              ),
-              FilledButton(onPressed: (){}, child: Icon(Icons.arrow_circle_right_outlined)),
-              SizedBox(height: 35),
-              Center(
-                child: RichText(text: TextSpan(
-                    text: "Have an account?",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600),
-                    children: [
-                      TextSpan(
-                          text: 'Login',style: TextStyle(color: AppColors.pcolor,fontWeight:FontWeight.bold),
-                          recognizer: TapGestureRecognizer()..onTap=_onTaplogin
-                      )
-                    ]
-        
-                )),
-              )
-        
-            ],
-          ),),
+            ),),
         ),
       ),
     );

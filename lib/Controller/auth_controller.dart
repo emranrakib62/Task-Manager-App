6b555eef39975408e812ccee4bd0e6993ea2657c
Emranul Hoque <emranrakib01862@gmail.com>
2026-06-10 16:16:25 +0600

    updated

--- lib/Controller/auth_controller.dart
+++ lib/Controller/auth_controller.dart
@@ -1,5 +1,22 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager/data/model/user_model.dart';
+
 class AuthController{
 static String _accessTokenKey='token';
-static String _accessTokenKey='token';
+static String _userModelKey='user_data';
+
+
+static String ? accessToken;
+static UserModel ? userModel;
+
+static Future saveUserData(UserModel model,String token) async {
+  SharedPreferences sharedPreferences=await SharedPreferences.getInstance();
+sharedPreferences.setString(_accessTokenKey, token);
+  sharedPreferences.setString(_userModelKey, jsonEncode(model.toJson()));
+accessToken=token;
+userModel=model;
+}
 
 }
